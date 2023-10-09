bd23be9688d6fa8f448b1f91a63cba65aef68217
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-10-09 13:45:44 -0400

    3rd div classname

--- src/index.js
+++ src/index.js
@@ -27,19 +27,24 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     )
-}
+  }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
