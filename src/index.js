49fad48cfd140d9c3c9fc6da73845b6b3ee4bc25
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-10-11 12:05:05 -0400

    jumpto(step)

--- src/index.js
+++ src/index.js
@@ -79,6 +79,13 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext
     });
   }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
