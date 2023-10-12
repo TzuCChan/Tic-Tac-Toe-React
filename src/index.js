30affb95e73a5e43d3a5f2d5c0953b5217e2ffd2
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-10-12 13:22:10 -0400

    else {}

--- src/index.js
+++ src/index.js
@@ -106,6 +106,8 @@ class Game extends React.Component {
     let status;
       if (winner) {
         status = "Winner: " + winner;
+      } else {
+        status = "Next player: " + (this.state.xIsNext ? "X" : "O");
       }
   }
 }
